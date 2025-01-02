54410abd81187fe7c53e251d6479fa7d316538e6
captainfaisal13 <shaikhfaisal3412@gmail.com>
2025-01-02 11:42:50 +0530

    feat: updated UI of signup and login page

--- app/(authentication)/signup/page.jsx
+++ app/(authentication)/signup/page.jsx
@@ -37,7 +37,7 @@ const SignUpPage = () => {
   };
 
   const onError = (error) => {
-    showAlert({ text: error.msg });
+    showAlert({ text: error?.response?.data?.msg });
   };
 
   const onSettled = () => {
@@ -58,61 +58,79 @@ const SignUpPage = () => {
   };
 
   return (
-    <div className="w-full h-screen flex justify-center items-center">
-      <div className="grid gap-4 text-center">
-        {alert.show && (
-          <p
-            className={`${
-              alert.type === "danger" ? "text-red-600" : "text-green-500"
-            }`}
-          >
-            {alert.text}
+    <div className="w-full h-screen grid grid-cols-1 md:grid-cols-5">
+      <div className="hidden md:col-span-2 pr-6 lg:pr-10 md:flex items-center">
+        <div className="ml-auto text-right">
+          <h1 className="text-text-2 text-3xl xl:text-5xl font-bold">
+            Create Account
+          </h1>
+          <p className="text-text-8 text-sm xl:text-lg font-medium">
+            Echo your voice in the infinite sphere!
           </p>
-        )}
-        <h1 className="font-bold text-4xl">Sign Up</h1>
-        <form
-          onSubmit={onSubmit}
-          className="md:w-96 grid gap-4 border p-4 rounded-sm"
-        >
-          <input
-            type="name"
-            name="name"
-            value={values.name}
-            onChange={handleChange}
-            placeholder="Name"
-            className="border p-2"
-          />
-          <input
-            type="username"
-            name="username"
-            value={values.username}
-            onChange={handleChange}
-            placeholder="Username"
-            className="border p-2"
-          />
-          <input
-            type="email"
-            name="email"
-            value={values.email}
-            onChange={handleChange}
-            placeholder="Email"
-            className="border p-2"
-          />
-          <input
-            type="password"
-            name="password"
-            value={values.password}
-            onChange={handleChange}
-            placeholder="Password"
-            className="border p-2"
-          />
-          <button type="submit" className="text-white bg-black py-2">
-            {loading ? <Loader classNames="m-0 size-6" /> : "Submit"}
-          </button>
-        </form>
-        <p>
-          Already have an account ? <Link href="/login">Login</Link>
-        </p>
+        </div>
+      </div>
+      <div className="md:col-span-3 flex items-center justify-center xl:justify-start border-l border-border-1 xl:pl-10">
+        <div className="md:w-96 text-center space-y-4 w-full px-10 md:px-0">
+          {alert.show && (
+            <p
+              className={` ${
+                alert.type === "danger" ? "text-red-600" : "text-green-600"
+              }`}
+            >
+              {alert.text}
+            </p>
+          )}
+          <h1 className="font-bold text-3xl text-text-2">Sign Up</h1>
+          <form
+            onSubmit={onSubmit}
+            className="grid gap-4 rounded-md border-border-1"
+          >
+            <input
+              type="name"
+              name="name"
+              value={values.name}
+              onChange={handleChange}
+              placeholder="Name"
+              className="py-2 px-4 rounded-lg bg-bg-4 placeholder:text-text-6 placeholder:font-thin focus:outline-border-3 outline-none text-text-1"
+            />
+            <input
+              type="username"
+              name="username"
+              value={values.username}
+              onChange={handleChange}
+              placeholder="Username"
+              className="py-2 px-4 rounded-lg bg-bg-4 placeholder:text-text-6 placeholder:font-thin focus:outline-border-3 outline-none text-text-1"
+            />
+            <input
+              type="email"
+              name="email"
+              value={values.email}
+              onChange={handleChange}
+              placeholder="Email"
+              className="py-2 px-4 rounded-lg bg-bg-4 placeholder:text-text-6 placeholder:font-thin focus:outline-border-3 outline-none text-text-1"
+            />
+            <input
+              type="password"
+              name="password"
+              value={values.password}
+              onChange={handleChange}
+              placeholder="Password"
+              className="py-2 px-4 rounded-lg bg-bg-4 placeholder:text-text-6 placeholder:font-thin focus:outline-border-3 outline-none text-text-1"
+            />
+            <button
+              type="submit"
+              className="bg-bg-5 text-text-0 py-4 rounded-lg"
+            >
+              {loading ? <Loader classNames="m-0 size-6" /> : "Submit"}
+            </button>
+          </form>
+          <p className="text-text-8">
+            Already have an account ?{" "}
+            <Link className="text-blue-400 underline" href="/login">
+              Login
+            </Link>
+          </p>
+        </div>
       </div>
     </div>
   );
