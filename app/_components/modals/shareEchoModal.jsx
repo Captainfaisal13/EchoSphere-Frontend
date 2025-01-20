1b393cc95bb4f5c3de8585b15fb352b79f210158
captainfaisal13 <shaikhfaisal3412@gmail.com>
2025-01-20 17:05:09 +0530

    feat: updated backend url on Share Echo Modal

--- app/_components/modals/shareEchoModal.jsx
+++ app/_components/modals/shareEchoModal.jsx
@@ -34,7 +34,7 @@ const ShareEchoModal = () => {
     dispatch(setShareEchoData(null));
   };
 
-  const echoUrl = `http://localhost:3000/${shareEchoData?.username}/echo/${shareEchoData?._id}`;
+  const echoUrl = `${process.env.NEXT_PUBLIC_BACKEND_URL}${shareEchoData?.username}/echo/${shareEchoData?._id}`;
   const handleShare = (platform) => {
     if (platform === "copy") {
       navigator.clipboard.writeText(echoUrl);
